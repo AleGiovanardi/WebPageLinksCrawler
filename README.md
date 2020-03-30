8f3b86e81f2a06d5eed5ecbec71beee88079bcc4
Alessandro Giovanardi <kranio@Alessandros-MacBook-Pro.local>
2020-03-30 19:09:40 +0200

    Func saveImage add

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ go get github.com/PuerkitoBio/goquery
 go build -o build/crawler main.go
 ./crawler anicepage.html https://www.google.com/data/hello.html
 ```
-On programs start three folder are created in working directory: Pages, Links, Logs folders.
+On program start three folder are created in working directory: Pages, Links, Logs folders.
 Saved file is saved in Pages folder. 
 A text file with links found in the crawled page is saved in Links folder.
 A logfile is saved for logging purposes in Logs folder.
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
@@ -14,10 +15,15 @@ import (
 	"github.com/dustin/go-humanize"
 )
 
+var (
+	fileName string
+)
+
+var URL = os.Args[2]
+
 //Func check checks for errors. If any panic and write log.
 func check(s string, e error) {
 	if e != nil {
-		panic(e)
 		log.Fatalln(e)
 	}
 }
@@ -46,9 +52,37 @@ func (wc WriteCounter) PrintProgress() {
 
 }
 
+//Func saveImage parse a file and reads line by line
+//A for loop is used to query and download images from their URLs.
+func saveImage() {
+	web, err := url.Parse(URL)
+	check("Error parsing URL ", err)
+	os.Chdir("imgs/" + web.Host + web.Path)
+	readFile, err := os.Open("imgs.txt")
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileTextLines []string
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	for _, eachline := range fileTextLines {
+		//os.Chdir("imgs/" + web.Host + web.Path)
+		//os.MkdirAll("imgs/"+web.Host+"/"+strings.Trim(eachline, "!https"), 0777)
+		res, err := http.Get(eachline)
+		check("", err)
+		file, err := os.OpenFile(".../imgs/"+web.Host+"/"+eachline, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+		io.Copy(file, res.Body)
+		check("", err)
+		file.Close()
+
+	}
+}
+
 //init func initializes directory creation and log tracing.
 func init() {
-	URL := os.Args[2]
+	//URL := os.Args[2]
 	dir, err := url.Parse(URL)
 	_, err = os.Stat("links/" + dir.Host + dir.Path + "/")
 	if os.IsNotExist(err) {
@@ -64,6 +98,11 @@ func init() {
 	if os.IsNotExist(err) {
 		errDir2 := os.MkdirAll("logs/"+dir.Host+"/", 0777)
 		check("Error creating log directory: ", errDir2)
+	}
+	_, err = os.Stat("imgs/" + dir.Host + dir.Path + "/")
+	if os.IsNotExist(err) {
+		errDir3 := os.MkdirAll("imgs/"+dir.Host+dir.Path, 0777)
+		check("Error creating imgs directory: ", errDir3)
 	}
 	//Log to file
 	file, e := os.OpenFile("logs/"+dir.Host+"/"+"file.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
@@ -87,7 +126,7 @@ func main() {
 	}
 
 	//Take URL argument from command line and parse.
-	URL := os.Args[2]
+	//URL := os.Args[2]
 	u, err := url.Parse(URL)
 
 	//Open file and append if it exist. If not create it and write.
@@ -140,6 +179,26 @@ func main() {
 			f.Close() // ignore error; Write error takes precedences
 			check("Error writing bytes to file: ", err)
 		}
+	})
+	//Find all images URLs and save to file
+	//We loop over each line of saved file
+	//to download each image by its URL
+	links.Find("img").Each(func(i int, s *goquery.Selection) {
+		src, exists := s.Attr("src")
+		if exists {
+			web, err := url.Parse(URL)
+			check("Error parsing URL ", err)
+			// If the file doesn't exist, create it, or else append to the file
+			f, err := os.OpenFile("/Users/kranio/Workspace/WebPageLinksCrawler/imgs/"+web.Host+web.Path+"imgs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+			check("Can't write image to disk.", err)
+			_, err = f.Write([]byte(src + "\n"))
+			f.Close() // ignore error; Write error takes precedences
+			check("Error writing bytes to file: ", err)
+
+		}
+
+		saveImage()
+
 	})
 	check("", err)
 	fmt.Println("\nCrawling finished.\n Success!")
